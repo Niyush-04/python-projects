58bba00ec15a6f8e122bd3d9f0b6680474d489b4
ItzMaurya <mauryapradip04@gmail.com>
2022-12-23 05:59:53 +0530

    few changes

--- SiCalculator.py
+++ SiCalculator.py
@@ -5,29 +5,27 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-import cmath
-
 
 # Create a class for the simple interest calculator
-class PolarFormConverter(GridLayout):
+class SimpleInterestCalculator(GridLayout):
 
     # Initialize the layout
     def __init__(self, **kwargs):
-        super(PolarFormConverter, self).__init__(**kwargs)
+        super(SimpleInterestCalculator, self).__init__(**kwargs)
         self.cols = 2
         
         # Add the necessary widgets
-        self.add_widget(Label(text="Realpart:"))
-        self.Realpart = TextInput(multiline=False)
-        self.add_widget(self.Realpart)
+        self.add_widget(Label(text="Principal:"))
+        self.principal = TextInput(multiline=False)
+        self.add_widget(self.principal)
         
-        self.add_widget(Label(text="imaginarypart:"))
-        self.imaginary = TextInput(multiline=False)
-        self.add_widget(self.imaginary)
+        self.add_widget(Label(text="Rate of Interest (%):"))
+        self.rate = TextInput(multiline=False)
+        self.add_widget(self.rate)
         
-        #self.add_widget(Label(text="Time (in years):"))
-        #self.time = TextInput(multiline=False)
-        #self.add_widget(self.time)
+        self.add_widget(Label(text="Time (in years):"))
+        self.time = TextInput(multiline=False)
+        self.add_widget(self.time)
         
         self.add_widget(Label(text="Simple Interest:"))
         self.simple_interest = Label(text="")
@@ -41,20 +39,18 @@ class PolarFormConverter(GridLayout):
         self.calculate.bind(on_press=self.calculate_simple_interest)
 
     # Define the function to calculate simple interest
-    def rectangular_to_polar(self, instance):
-        Realpart = float(self.Realpart.text)
-        imaginary = float(self.imaginary.text)
-        angle = cmath.phase(self, instance)
-        magnitude = abs(self, instance)
-       
+    def calculate_simple_interest(self, instance):
+        principal = float(self.principal.text)
+        rate = float(self.rate.text)
+        time = float(self.time.text)
         
-        rectangular_to_polar = (magnitude, angle*180/3.14)
-        self.rectangular_to_polar.text = str(rectangular_to_polar)
+        simple_interest = (principal * rate * time) / 100
+        self.simple_interest.text = str(simple_interest)
 
 # Create the app class
 class SimpleInterestApp(App):
     def build(self):
-        return PolarFormConverter()
+        return SimpleInterestCalculator()
 
 # Run the app
 if __name__ == "__main__":
